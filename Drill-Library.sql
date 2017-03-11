16a811f0eecf21e210af8b8458768d5d2b2c62ab
Bryce Tucker <bryce.tucker@gmail.com>
2017-03-10 19:49:48 -0800

    Added printouts of tables, populated some more of the tables with content.

--- Drill-Library.sql
+++ Drill-Library.sql
@@ -52,7 +52,7 @@ CREATE TABLE BOOK_LOANS
 
 CREATE TABLE BORROWER
 	(
-		CardNo int PRIMARY KEY NOT NULL IDENTITY(1,1),
+		CardNo int PRIMARY KEY NOT NULL,
 		Name varchar (50),
 		BorrowerAddress varchar (100),
 		Phone varchar (15)
@@ -159,9 +159,36 @@ INSERT INTO PUBLISHER VALUES
 
 
 INSERT INTO LIBRARY_BRANCH VALUES
-(),
+(101, 'Central', '352 E Main St., Hermiston, OR 97838'),
+(102, 'Sharpstown', '356 W Fifth Ave., Boardman, OR 95882'),
+(103, 'Frederick', '1032 23rd St., Eugene, OR 97243'),
+(104, 'Elbow', '567 E Middleton Ave., Bend, OR 97943'),
+(105, 'International Local 505', '34 W East St., Condon, OR 97774'),
+(106, 'Adjacent', '562 S 7th Ave., Oregon City, OR 97109')
 
 
+
+INSERT INTO BORROWER VALUES
+(1, 'Joe Bob', '56 S 56th St, Eugene, OR 97243', '(503) 764-9128'),
+(2, 'Billy Todd', '2 N 2nd Ave, Hermiston, OR 97841', '(234) 724-1234'),
+(3, 'Janet Malloy', '14 S 14th Ave, Boardman, OR 95882', '(234) 764-2345'),
+(4, 'Sis Teach', '13 E 13th St, Bend OR 98734', '(234) 325-3466'),
+(5, 'Deveroe Heath', '23 N 23rd Ave, Oregon City OR 91536', '(435) 532-4576'),
+(6, 'Malachi Devenride', '1010 W East St, Hermiston OR 97838', '(344) 112-5678'),
+(7, 'Esther Von Brawm', '34512 Main St, Portland Or 97145', '(876) 345-6789'),
+(8, 'Vincent Powers', '50 N 123 Ave, Condon OR 97465', '(675) 523-8904'),
+(9, 'John Eventrowel', '10 S Bismark St, Eugene OR 93645', '(235) 456-3457'),
+(10, 'Evan Weygant', '34 N 3rd Ave, Bend OR 98734', '(453) 764-5685')
+
+
+
+
+
+
+
+SELECT * FROM LIBRARY_BRANCH
+
+SELECT * FROM BORROWER
 
 SELECT * FROM BOOK AS B INNER JOIN BOOK_AUTHORS AS A ON B.BookID=A.BookID
 INNER JOIN PUBLISHER AS P ON B.PublisherName=P.Name
